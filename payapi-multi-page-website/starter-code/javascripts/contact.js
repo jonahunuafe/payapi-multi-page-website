cfb965cba4ed01d375dc0173ccceca531efb92f3
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-19 09:46:18 +0100

    added validation to contact page

---
+++ payapi-multi-page-website/starter-code/javascripts/contact.js
@@ -0,0 +1,19 @@
+const form = document.querySelector(".form");
+const email = document.querySelector("#email");
+
+
+function preventSubmit(event) {
+    event.preventDefault();
+}
+
+function validateEmail() {
+    if (email.value === "") {
+      document.querySelector(".error-msg").innerHTML = "This field can't be empty";
+      
+    } else {
+      document.querySelector(".error-msg").innerHTML = "";
+    }
+}
+
+form.addEventListener("submit", preventSubmit);
+email.addEventListener("input", validateEmail);
